affa532650abdf71ee812314e69cb30081f91cfc
MhZ9191 <hegic91@gmail.com>
2026-04-27 12:21:15 +0200

    Fix error & add BigDecimal

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,13 +1,16 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         
-        Prodotto test = new Prodotto("test","Hello World!",33.5F);
+        Prodotto test = new Prodotto("lampadina","Hello World!",new BigDecimal(10),new BigDecimal(22));
 
-        System.out.printf("Prezzo base -> %.2f%n",test.priceBase());
-        System.out.printf("Prezzo piu' iva -> %.2f%n",test.pricePlusIva());
+
         System.out.printf("Nome esteso -> %s%n",test.extendedName());
+        System.out.printf("Prezzo base -> %.2f $%n",test.getPriceBase());
+        System.out.printf("Prezzo con iva -> %.2f $%n",test.getPricePlusIva());
 
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,32 +1,40 @@
 package org.lessons.java.shop;
 
 import java.util.Random;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Prodotto {
     public int code;
     public String name;
     public String description;
-    public float price;
-    public int iva;
+    public BigDecimal price;
+    public BigDecimal iva;
 
-    public Prodotto(String name,String description,float price){
+    public Prodotto(String name,String description,BigDecimal price,BigDecimal iva){
         Random rnd = new Random();
         this.code=rnd.nextInt(99999);
         this.name=name;
         this.description=description;
         this.price=price;
-        this.iva=22;    
+        this.iva=iva;    
     }
 
-    float priceBase(){
+    public BigDecimal getPriceBase(){
         return this.price;
     }
 
-    float pricePlusIva(){
-        return this.price+((this.price*this.iva)/100);
+    public BigDecimal getPricePlusIva(){
+        if(price!=null && iva !=null){
+            return price.add(price.multiply(iva).divide(new BigDecimal(100))).setScale(2,RoundingMode.DOWN);
+        }
+        return null;
     }
 
-    String extendedName(){
-        return this.code+"-"+this.name;
+    public String extendedName(){
+        if(name!=null){
+            return this.code+" - "+this.name;
+        }
+        return null;
     }
 }
